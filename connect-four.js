f58aef3fc8f855a1ba4a490db867047f22dff99a
Mustafa <mustafaomousa@icloud.com>
2020-10-08 12:45:15 -0500

    Start A New Game Complete

--- connect-four.js
+++ connect-four.js
@@ -1,29 +1,44 @@
-import { Game } from "./game.js";
+import Game  from "./game.js";
 
 let game = undefined;
+
+function updateUI() {
+    let boardHolder = document.getElementById('board-holder');
+    let gameName = document.getElementById('game-name');
+    if(game === undefined){
+        boardHolder.className = 'is-invisible';
+    } else {
+        boardHolder.className = '';
+        gameName.innerHTML = game.getName();
+    }
+}
+
 window.addEventListener("DOMContentLoaded", event => {
+    let newGame = document.getElementById("new-game");
     let playerName = document.getElementById("player-1-name");
-    playerName.addEventListener("keyup", event => {
-        let newGame = document.getElementById("new-game");
-        if(player1 !== "" && player2 !== "") {
-            newGame.setAttribute("disabled", "false");
-        } else {
-            newGame.setAttribute("disabled", "true");
-        }
-    });
     let player2Name = document.getElementById("player-2-name");
-    player2Name.addEventListener("keyup", event => {
-        let newGame = document.getElementById("new-game");
-        if (player1 !== "" && player2 !== "") {
-            newGame.setAttribute("disabled", "false");
+
+    playerName.addEventListener("keyup", event => {
+        if(playerName.value !== "" && player2Name.value !== "") {
+            newGame.disabled = false;
         } else {
-            newGame.setAttribute("disabled", "true");
+            newGame.disabled = true;
         }
     });
-    document.getElementById("new-game")
-    .addEventListener("click", event => {
-        this.Game(playerName.value, player2Name.value);
+
+    player2Name.addEventListener("keyup", event => {
+        if (playerName.value !== "" && player2Name.value !== "") {
+            newGame.disabled = false;
+        } else {
+            newGame.disabled = true;
+        }
+    });
+
+    newGame.addEventListener("click", event => {
+        game = new Game(playerName.value, player2Name.value);
         playerName.value = "";
-        player2Name.value = ""; 
+        player2Name.value = "";
+        newGame.disabled = true;
+        updateUI();
     })
 });
