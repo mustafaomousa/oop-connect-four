52c942f8cf639965d7bf403a185723341361f854
Daniel Miller <dbmiller25@gmail.com>
2020-10-08 18:39:42 -0400

    Started fixing Column wins

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -9,10 +9,10 @@ export default class ColumnWinInspector {
         for(let rowNum = 0; rowNum <= 2; rowNum++) {
             console.log(this.col);
             //console.log(this.col[rowNum], this.col[rowNum + 1], this.col[rowNum + 2], this.col[rowNum+3])
-                if ((this.col[rowNum] === 1)
-                && (this.col[rowNum + 1] === 1)
-                && (this.col[rowNum + 2] === 1) 
-                && (this.col[rowNum + 3] === 1)){
+                if ((this.col.getTokenAt(rowNum) === 1)
+                    && (this.col.getTokenAt(rowNum+1) === 1)
+                    && (this.col.getTokenAt(rowNum+2) === 1) 
+                    && (this.col.getTokenAt(rowNum+3) === 1)){
                     console.log("I'm here");
                     return true; 
                 }
